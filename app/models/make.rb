9186cd162a5650185b4d1bdd4cd9c9662b55595d
esoft45 - Patricia Luz Bastos <patricia.bastos90@gmail.com>
2016-09-25 18:56:31 -0300

    trazendo toda a regra de requisição das apis para o model + adicionando active record relation + first_or_create para melhorar performance

--- app/models/make.rb
+++ app/models/make.rb
@@ -1,2 +1,17 @@
 class Make < ActiveRecord::Base
+  has_many :models
+
+  def self.sync_api
+    #search the make
+    uri = URI("http://www.webmotors.com.br/carro/marcas")
+
+    # Make request for Webmotors site
+    response = Net::HTTP.post_form(uri, {})
+    json = JSON.parse response.body
+
+    # Itera no resultado e grava as marcas que ainda não estão persistidas
+    json.each do |make_params|
+      make = Make.where(name: make_params["Nome"], webmotors_id: make_params["Id"]).first_or_create
+    end
+  end
 end
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,2 +1,16 @@
 class Model < ActiveRecord::Base
+  belongs_to :make
+
+  def self.sync_api(make)
+    #search the models
+    uri = URI("http://www.webmotors.com.br/carro/modelos")
+
+    response = Net::HTTP.post_form(uri, { marca: make.id })
+    models_json = JSON.parse response.body
+
+    # Itera no resultado e grava os modelos que ainda não estão persistidas
+    models_json.each do |json|
+      Model.where(name: json["Nome"], make_id: make.id).first_or_create
+    end
+  end
 end
